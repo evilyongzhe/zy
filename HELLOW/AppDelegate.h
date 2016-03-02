f5090a8f125c166ac31d57ce58c6eca2309589c0
evilyongzhe <zy614125682@icloud.com>
2016-03-02 13:27:20 -0800

    Initial Commit

---
+++ HELLOW/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HELLOW
+//
+//  Created by 张羽 on 16/3/2.
+//  Copyright © 2016年 张羽. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HELLOW/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HELLOW
+//
+//  Created by 张羽 on 16/3/2.
+//  Copyright © 2016年 张羽. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
